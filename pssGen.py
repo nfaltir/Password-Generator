5282f6767d9874aebef93dc69a02d9ea8ab50806
nfaltir <nfaltir@protonmail.com>
2020-01-08 22:49:16 -0800

    added a for loop to control iteration count

--- pssGen.py
+++ pssGen.py
@@ -1,11 +1,20 @@
 import random
 
-#Asks user to generate bitcoin password
+# Asks user to generate bitcoin password
 print("\n===============BITCOIN PASSWROD GENERATOR===============")
-#asks user to enter password length
+# asks user to enter password length
 passLength = int(input("Enter length of password:"))
+
+
 string = "abcdefghijklmnABCoZ!@#$%^xyz012345678MpqrsDLEF0AZ!@#$%^BCODtKuvJwIGHxyz01234567890AZ!@#$%^BCODEFGHNIJKLMNOPQRSTUVWXYZ!@#$%^%^&*()?"
 #passLength = 20
-password =  "".join(random.sample(string,passLength))
+passwordCount = int(input("Enter how many passwords you want to generate?:"))
+password = "".join(random.sample(string, passLength))
 
-print ("\nNew password: ", password)
+#controlls how many iterations
+for password in range(passwordCount):
+    password = "".join(random.sample(string, passLength))
+    print("\nNew password: ", password)
+
+
+#print ("\nNew password: ", password)
